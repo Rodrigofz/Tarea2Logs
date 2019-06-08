16fbd9f0aee6d99d8e03e59b9e9a5d7726476885
Rodrigo Fuentes <rodrigo.fuentes.z@ug.uchile.cl>
2019-06-07 20:25:58 -0400

    Comienzo de algoritmo de Ukonnen

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,47 @@
 import edge.*;
 import node.*;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String Args[]){
-        Node root = new Node();
-        Node node1 = new Node();
-        root.link(node1, "jaja");
-        Node node2 = new Node();
-        root.link(node2, "jojo");
-        Node node3 = new Node();
-        node1.link(node3, "jiji");
-        root.printTree(0);
+        Node tree = buildTree("banana");
+        tree.printTree(0);
     }
 
     public static Node buildTree(String word){
+        word = word + "$";
         Node root = new Node();
         Node active_node = root;
         String active_edge = "";
         int active_length = 0;
         int remainder = 0;
 
-        Node node = new Node();
-        for(int i=0; i<word.length(); i++){
-            char c = word.charAt(i);
+        ArrayList<Edge> leafEdges = new ArrayList<Edge>();
 
+        for(int i=0; i<word.length(); i++) {
+            //Obtener caracteres
+            char c = word.charAt(i);
+            String c_str = "" + c;
+            //Annadir caracter a cada link que lleva a hoja
+            for (Edge e : leafEdges) {
+                e.setLabel(e.getLabel() + c);
+            }
+
+            //Verificar si el sufijo esta incluido en el arbol
+            boolean in_tree = false;
+            for (Edge e : active_node.getEdges()) {
+                if (e.getLabel().startsWith(c_str)) {
+                    in_tree = true;
+                }
+            }
+            if (!in_tree) {
+                Node new_node = new Node();
+                Edge new_edge = root.link(new_node, c_str);
+                leafEdges.add(new_edge);
+            } else {
+
+            }
         }
         return root;
     }
