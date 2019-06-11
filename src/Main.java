1a79cf4cc1d4b45b8b87f89ab8a9fafdcbb4a6fe
Rodrigo Fuentes <rodrigo.fuentes.z@ug.uchile.cl>
2019-06-10 20:28:28 -0400

    Construccion del arbol en proceso

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import active_point.ActivePoint;
 import edge.*;
 import node.*;
 
@@ -5,43 +6,29 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String Args[]){
-        Node tree = buildTree("banana");
-        tree.printTree(0);
+        Node root = buildTree("banana");
+        root.printTree(0);
     }
 
     public static Node buildTree(String word){
         word = word + "$";
         Node root = new Node();
-        Node active_node = root;
-        String active_edge = "";
-        int active_length = 0;
+        ActivePoint ap = new ActivePoint(root);
         int remainder = 0;
-
-        ArrayList<Edge> leafEdges = new ArrayList<Edge>();
 
         for(int i=0; i<word.length(); i++) {
             //Obtener caracteres
             char c = word.charAt(i);
             String c_str = "" + c;
             //Annadir caracter a cada link que lleva a hoja
-            for (Edge e : leafEdges) {
+            for (Edge e : ap.leafEdges) {
                 e.setLabel(e.getLabel() + c);
             }
 
-            //Verificar si el sufijo esta incluido en el arbol
-            boolean in_tree = false;
-            for (Edge e : active_node.getEdges()) {
-                if (e.getLabel().startsWith(c_str)) {
-                    in_tree = true;
-                }
-            }
-            if (!in_tree) {
-                Node new_node = new Node();
-                Edge new_edge = root.link(new_node, c_str);
-                leafEdges.add(new_edge);
-            } else {
+            //Intentar insertar en el active point
+            ap = ap.insert(c);
 
-            }
+
         }
         return root;
     }
